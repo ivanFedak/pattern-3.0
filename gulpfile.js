c26f8f152142c0339ab7a40598ffbb399cb979c1
admin <admin@admins-MacBook-Air.local>
2021-12-27 13:31:03 +0200

    finish

--- gulpfile.js
+++ gulpfile.js
@@ -23,13 +23,15 @@ import {js} from './gulp/tasks/js.js';
 import {images} from './gulp/tasks/images.js';
 import {otfToTtf, ttfToWoff, fontsStyle} from './gulp/tasks/fonts.js';
 import {svgSprive} from './gulp/tasks/svgSprive.js';
+import {zip} from './gulp/tasks/zip.js';
+import {ftp} from './gulp/tasks/ftp.js';
 
 //Наблюдение за изминениями файлов
 function watcher() {
     gulp.watch(path.watch.files, copy);
-    gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.html, html); //gulp.series(html,ftp) если хотим чтобы при изменении файла сразу постился на сервер
     gulp.watch(path.watch.sass, sass);
-    gulp.watch(path.watch.js, js);
+    gulp.watch(path.watch.js, js); 
     gulp.watch(path.watch.images, images);
 }
 
@@ -44,10 +46,14 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy,html,sass,js, images));
 //Построение сценария выполнения задач
 const dev = gulp.series(reset,mainTasks,gulp.parallel(watcher,server));
 const build = gulp.series(reset, mainTasks);
+const deployZIP = gulp.series(reset, mainTasks, zip); 
+const deployFTP = gulp.series(reset, mainTasks, ftp); 
 
 //Экспорт сценариев
 export {dev};//npm run dev
 export {build}; //npm run build
+export {deployZIP}; //npm run zip
+export {deployFTP}; //npm run deploy
 
 //Выполняем сценарий по умолчанию
 gulp.task('default', dev);//gulp
